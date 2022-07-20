2d62dea83830d9d02c36178e1e530dc5040ae54d
Matthias231 <60029799+Matthias231@users.noreply.github.com>
2022-07-20 14:04:09 +0200

    minor changes to template files

--- batch_files/create_all_planforms.bat
+++ batch_files/create_all_planforms.bat
@@ -1,5 +1,5 @@
-python scripts\planform_creator.py -p ressources/planformdata_tail 
+python scripts\planform_creator.py -p ressources/planformdata_wing 
 copy build\planforms\XFLR5_wing.xml ressources\XFLR5_wing.xml 
 copy build\planforms\FLZ_wing.flz ressources\FLZ_wing.flz 
-python scripts\planform_creator.py -p ressources/planformdata_wing 
-
+python scripts\planform_creator.py -p ressources/planformdata_tail 
+pause 
