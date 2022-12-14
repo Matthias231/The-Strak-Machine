135191387a6937e7c0ff64d486f1b0373ecfcc9e
Matthias231 <60029799+Matthias231@users.noreply.github.com>
2022-12-14 18:01:45 +0100

    Update pip_install.bat

--- batch_files/pip_install.bat
+++ batch_files/pip_install.bat
@@ -6,7 +6,6 @@ pip3 install f90nml
 pip3 install scipy
 pip3 install matplotlib
 pip3 install --upgrade matplotlib
-pip3 install customtkinter
-pip3 install --upgrade customtkinter
+pip3 install customtkinter==4.6.3
 pip3 install ezdxf
 pause
