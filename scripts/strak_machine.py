a6e4c78159ac7c067075b6c55c034d59d7ecda2d
Matthias231 <60029799+Matthias231@users.noreply.github.com>
2022-12-16 16:46:41 +0100

    Update strak_machine.py
    workaround for xfoil worker problem

--- scripts/strak_machine.py
+++ scripts/strak_machine.py
@@ -4429,11 +4429,6 @@ class strak_machine:
         (self.params.xfoilWorkerCall, thickPos, rootfoilName, seedfoilPrefix))
         system(systemString)
 
-        # set thickness
-        systemString = ("%s -w set t=%.2f -a %s -o %s\n\n" %\
-        (self.params.xfoilWorkerCall, thick, seedfoilName, seedfoilPrefix))
-        system(systemString)
-
         # set camber
         systemString = ("%s -w set c=%.2f -a %s -o %s\n\n" %\
         (self.params.xfoilWorkerCall, camb, seedfoilName, seedfoilPrefix))
@@ -4442,6 +4437,11 @@ class strak_machine:
         # set camber position
         systemString = ("%s -w set xc=%.2f -a %s -o %s\n\n" %\
         (self.params.xfoilWorkerCall, cambPos, seedfoilName, seedfoilPrefix))
+        system(systemString)
+
+        # set thickness
+        systemString = ("%s -w set t=%.2f -a %s -o %s\n\n" %\
+        (self.params.xfoilWorkerCall, thick, seedfoilName, seedfoilPrefix))
         system(systemString)
 
         NoteMsg("seedfoil %s was successfully generated" % seedfoilName)
